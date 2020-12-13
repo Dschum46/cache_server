411f5e9273c08e41f31869d525a8843e322312dc
DmytroGnatiuk <dschum46@gmail.com>
2020-12-13 15:15:08 +0200

    cache_server added

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # cache_server
 Betinvest lesson 7 cache server
+
+wget https://erlang.mk/erlang.mk
+
+make -f erlang.mk bootstrap-lib
+
+make bootstrap-rel
+
+make
+
+make run
---
+++ src/cache_server_app.erl
@@ -0,0 +1,19 @@
+-module(cache_server_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+start(_, _) ->
+    Dispatch = cowboy_router:compile([
+		{'_', [
+			{"/api/cache_server", cache_server_http, []}
+		]}
+	]),
+
+	{ok, _Pid} = cowboy:start_clear(http, [{port, 8080}], #{env => #{dispatch => Dispatch}}),
+    cache_server_sup:start_link().
+
+stop(_State) ->
+    ok = cowboy:stop_listener(http).
